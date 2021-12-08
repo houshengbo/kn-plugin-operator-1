727ef1fd54440ed4e1e7bca912f383e283019f62
Vincent Hou <shou@us.ibm.com>
2021-12-08 12:39:26 -0500

    Add serving installation

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module knative.dev/kn-plugin-operator
 go 1.16
 
 require (
+	github.com/ghodss/yaml v1.0.0
 	github.com/k14s/ytt v0.37.0
 	github.com/manifestival/client-go-client v0.5.0
 	github.com/spf13/cobra v1.2.1
-	golang.org/x/mod v0.4.2
+	golang.org/x/mod v0.5.1
 	k8s.io/api v0.22.2
 	k8s.io/apimachinery v0.22.2
 	k8s.io/client-go v0.22.2
-	knative.dev/hack v0.0.0-20211122162614-813559cefdda
+	knative.dev/hack v0.0.0-20211207151905-50bb1cbe334c
+	knative.dev/operator v0.27.1-0.20211207155058-6d0ea120321f
 )
--- pkg/command/install/install.go
+++ pkg/command/install/install.go
@@ -26,12 +26,34 @@ import (
 	"knative.dev/kn-plugin-operator/pkg/command/common"
 )
 
+const IstioDefaultNamespace = "istio-system"
+
 type installCmdFlags struct {
 	Component      string
 	IstioNamespace string
 	Namespace      string
 	KubeConfig     string
 	Version        string
+}
+
+func (flags *installCmdFlags) fill_defaults() {
+	if flags.Version == "" {
+		flags.Version = "latest"
+	}
+
+	if flags.IstioNamespace == "" {
+		flags.IstioNamespace = IstioDefaultNamespace
+	}
+
+	if flags.Namespace == "" {
+		if strings.EqualFold(flags.Component, "serving") {
+			flags.Namespace = common.KnativeServingName
+		} else if strings.EqualFold(flags.Component, "eventing") {
+			flags.Namespace = common.KnativeEventingName
+		} else if flags.Component == "" {
+			flags.Namespace = "default"
+		}
+	}
 }
 
 var (
@@ -48,7 +70,13 @@ func NewInstallCommand(p *pkg.OperatorParams) *cobra.Command {
   kn operation install -c serving --namespace knative-serving`,
 
 		RunE: func(cmd *cobra.Command, args []string) error {
+			installFlags.fill_defaults()
 			client, err := p.NewKubeClient()
+			if err != nil {
+				return fmt.Errorf("cannot get source cluster kube config, please use --kubeconfig or export environment variable KUBECONFIG to set\n")
+			}
+
+			operatorClient, err := p.NewOperatorClient()
 			if err != nil {
 				return fmt.Errorf("cannot get source cluster kube config, please use --kubeconfig or export environment variable KUBECONFIG to set\n")
 			}
@@ -71,18 +99,35 @@ func NewInstallCommand(p *pkg.OperatorParams) *cobra.Command {
 				return err
 			}
 
-			yamlTemplateString, err := common.DownloadFile(URL)
-			if err != nil {
-				return err
+			yamlTemplateString := ""
+			overlayContent := ""
+			yamlValuesContent := ""
+
+			if installFlags.Component != "" {
+				ksCR := common.KnativeOperatorCR{
+					KnativeOperatorClient: operatorClient,
+				}
+				kCR, err := ksCR.GetCRInterface(installFlags.Component, installFlags.Namespace)
+				if err != nil {
+					return err
+				}
+				yamlGenerator := common.YamlGenarator{
+					Input: kCR,
+				}
+				yamlTemplateString, err = yamlGenerator.GenerateYamlOutput()
+				if err != nil {
+					return err
+				}
+			} else {
+				// Install the Knative Operator
+				yamlTemplateString, err = common.DownloadFile(URL)
+				if err != nil {
+					return err
+				}
 			}
 
-			operatorOverlay := rootPath + "/overlay/operator.yaml"
-			overlayContent, err := common.ReadFile(operatorOverlay)
-			if err != nil {
-				return err
-			}
-			yamlValuesContent := fmt.Sprintf("#@data/values\n---\nnamespace: %s", installFlags.Namespace)
-
+			overlayContent = getOverlayYamlContent(installFlags, rootPath)
+			yamlValuesContent = getYamlValuesContent(installFlags)
 			yttp := common.YttProcessor{
 				BaseData:    []byte(yamlTemplateString),
 				OverlayData: []byte(overlayContent),
@@ -109,7 +154,7 @@ func NewInstallCommand(p *pkg.OperatorParams) *cobra.Command {
 	installCmd.Flags().StringVarP(&installFlags.Namespace, "namespace", "n", "", "The namespace of the Knative Operator or the Knative component")
 	installCmd.Flags().StringVarP(&installFlags.Component, "component", "c", "", "The name of the Knative Component to install")
 	installCmd.Flags().StringVarP(&installFlags.Version, "version", "v", "latest", "The version of the the Knative Operator or the Knative component")
-	installCmd.Flags().StringVar(&installFlags.IstioNamespace, "istio-namespace", "", "The namespace of istio")
+	installCmd.Flags().StringVar(&installFlags.IstioNamespace, "istio-namespace", IstioDefaultNamespace, "The namespace of istio")
 
 	return installCmd
 }
@@ -132,4 +177,42 @@ func getOperatorURL(version string) (string, error) {
 		URL = fmt.Sprintf("https://github.com/knative/operator/releases/download/%s%s/operator.yaml", prefix, versionSanitized)
 	}
 	return URL, nil
+}
+
+func getOverlayYamlContent(installFlags installCmdFlags, rootPath string) string {
+	path := ""
+	if strings.EqualFold(installFlags.Component, "serving") {
+		path = rootPath + "/overlay/ks.yaml"
+		if installFlags.IstioNamespace != IstioDefaultNamespace {
+			path = rootPath + "/overlay/ks_istio_ns.yaml"
+		}
+	} else if strings.EqualFold(installFlags.Component, "eventing") {
+		path = rootPath + "/overlay/ke.yaml"
+	} else if installFlags.Component == "" {
+		path = rootPath + "/overlay/operator.yaml"
+	}
+
+	if path == "" {
+		return ""
+	}
+	overlayContent, _ := common.ReadFile(path)
+	return overlayContent
+}
+
+func getYamlValuesContent(installFlags installCmdFlags) string {
+	content := ""
+	if strings.EqualFold(installFlags.Component, "serving") {
+		content = fmt.Sprintf("#@data/values\n---\nname: %s\nnamespace: %s\nversion: '%s'",
+			common.KnativeServingName, installFlags.Namespace, installFlags.Version)
+		if installFlags.IstioNamespace != IstioDefaultNamespace {
+			myslice := []string{content, fmt.Sprintf("local_gateway_value: knative-local-gateway.%s.svc.cluster.local", installFlags.IstioNamespace)}
+			content = strings.Join(myslice, "\n")
+		}
+	} else if strings.EqualFold(installFlags.Component, "eventing") {
+		content = fmt.Sprintf("#@data/values\n---\nname: %s\nnamespace: %s\nversion: '%s'",
+			common.KnativeEventingName, installFlags.Namespace, installFlags.Version)
+	} else if installFlags.Component == "" {
+		content = fmt.Sprintf("#@data/values\n---\nnamespace: %s", installFlags.Namespace)
+	}
+	return content
 }
